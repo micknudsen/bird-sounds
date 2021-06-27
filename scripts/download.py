5ec3fe0721373bc8abfc087b124bc42fe3c14465
Michael Knudsen <micknudsen@gmail.com>
2021-06-27 20:27:56 +0200

    Download a few well-known species

--- scripts/download.py
+++ scripts/download.py
@@ -1,9 +1,14 @@
+import json
+import logging
 import os
-import urllib
+import urllib.request
 
 import pandas as pd
 from tqdm import tqdm
 
+
+logging.basicConfig(format='[%(levelname)s] %(asctime)s %(message)s',
+                    datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO)
 
 meatadata = pd.read_csv('xeno-canto/verbatim.txt', sep='\t', low_memory=False)
 meatadata = meatadata[['gbifID', 'behavior', 'scientificName', 'vernacularName']].set_index('gbifID')
@@ -27,5 +32,14 @@ def download(data, species, behavior):
         local_file = os.path.join('..', 'static', 'sounds', simple_species, simple_behavior, f'{gbif_id}.mp3')
 
         if not os.path.isfile(local_file):
-            os.makedirs(os.path.dirname(local_file), exist_ok=True)
-            urllib.request.urlretrieve(download_link, local_file)
+            try:
+                os.makedirs(os.path.dirname(local_file), exist_ok=True)
+                urllib.request.urlretrieve(download_link, local_file)
+            except urllib.error.HTTPError as e:
+                logging.warning(f'Unable to download sound for {species} ({behavior}) with GBIF_ID {gbif_id}. Reason given: {e}')
+
+
+with open('selection.json', 'r') as f:
+    for species, behaviors in json.load(f).items():
+        for behavior in behaviors:
+            download(data=data, species=species, behavior=behavior)
