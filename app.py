b0f2611e9c7f6c5859aa2f285f4090259c026e52
Michael Knudsen <micknudsen@gmail.com>
2021-06-27 20:45:25 +0200

    Create dictionary with paths to sounds (relative to static folder)

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+import json
+import pathlib
+
+from collections import defaultdict
+
 from flask import Flask
 
 
 app = Flask(__name__)
+
+
+with open('metadata/translation.json', 'r') as f:
+    translation = {key.replace(' ', '_').lower(): value
+                   for key, value in json.load(f).items()}
+
+sounds = defaultdict(list)
+for path in pathlib.Path('static/sounds').rglob('*.mp3'):
+    sounds[path.parts[2]].append(str(path).removeprefix('static/'))
 
 
 @app.route('/')
