397392f1480d460a30ae1689fab1a7a7db929e6d
Michael Knudsen <micknudsen@gmail.com>
2021-06-29 22:26:45 +0200

    Time to rethink this

--- app.py
+++ app.py
@@ -1,28 +1,8 @@
-import json
-import pathlib
-import random
-
-from collections import defaultdict
-
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-
-with open('metadata/translation.json', 'r') as f:
-    translation = {key.replace(' ', '_').lower(): value
-                   for key, value in json.load(f).items()}
-
-sounds = defaultdict(list)
-for path in pathlib.Path('static/sounds').rglob('*.mp3'):
-    sounds[path.parts[2]].append(str(path).removeprefix('static/'))
-
-
 @app.route('/')
 def index():
-
-    species = random.choice(list(sounds.keys()))
-    sound_url = url_for('static', filename=random.choice(sounds[species]))
-
-    return render_template('index.html', sound_url=sound_url)
+    return render_template('index.html')
