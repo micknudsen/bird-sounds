355866a2e1c50449b4666b77530ff4d893a242b9
Michael Knudsen <micknudsen@gmail.com>
2021-06-30 20:34:18 +0200

    Vernacular name is not used

--- metadata/download.py
+++ metadata/download.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='[%(levelname)s] %(asctime)s %(message)s',
                     datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO)
 
 meatadata = pd.read_csv('xeno-canto/verbatim.txt', sep='\t', low_memory=False)
-meatadata = meatadata[['gbifID', 'behavior', 'scientificName', 'vernacularName']].set_index('gbifID')
+meatadata = meatadata[['gbifID', 'behavior', 'scientificName']].set_index('gbifID')
 
 multimedia = pd.read_csv('xeno-canto/multimedia.txt', sep='\t', low_memory=False)
 multimedia = multimedia[multimedia['type'] == 'Sound'][['gbifID', 'identifier']].set_index('gbifID')
