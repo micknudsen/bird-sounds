4c5b4a5eb3a58737d893fccb0f0ffe01cda3f890
Michael Knudsen <micknudsen@gmail.com>
2021-06-30 21:31:03 +0200

    Implemented Species class

--- app.py
+++ app.py
@@ -8,10 +8,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+class Species(db.Model):
+
+    __tablename__ = 'species'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self) -> str:
+        return f'<Species {self.name}>'
 
 
 @app.route('/')
