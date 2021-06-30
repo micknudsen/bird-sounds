a4cc0968820fdf28bdb0516cd23e2453c2011765
Michael Knudsen <micknudsen@gmail.com>
2021-06-30 21:12:37 +0200

    Configured SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+import os
 
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
