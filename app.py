58249373ea130f8f847ca30056c648bbbf1291a2
Michael Knudsen <micknudsen@gmail.com>
2021-07-24 09:16:18 +0200

    Starting over and doing things right(ish)

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ class Species(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     sounds = db.relation('Sound', backref='species')
-    guesses = db.relation('Guess', backref='species')
 
 
 class Behavior(db.Model):
@@ -40,85 +39,8 @@ class Sound(db.Model):
     path = db.Column(db.String(64), unique=True)
     species_id = db.Column(db.Integer, db.ForeignKey('species.id'))
     behavior_id = db.Column(db.Integer, db.ForeignKey('behavior.id'))
-    guesses = db.relation('Guess', backref='sound')
 
 
-class Guess(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sound_id = db.Column(db.Integer, db.ForeignKey('sound.id'))
-    species_id = db.Column(db.Integer, db.ForeignKey('species.id'))
-
-
-def get_sound() -> Sound:
-
-    candidate_species = Species.query.all()
-
-    wrong_counts = []
-    for species in candidate_species:
-        wrong_counts.append(sum(1 for guess in Guess.query.filter_by(species=species)
-                                if not guess.species == guess.sound.species))
-
-    weights = [1 + count for count in wrong_counts]
-    selected_species = random.choices(candidate_species, weights=weights, k=1)[0]
-
-    return random.sample(Sound.query.filter_by(species=selected_species).all(), k=1)[0]
-
-
-def get_choices(sound: Sound) -> List[Species]:
-
-    alternatives = [alternative for alternative in Species.query.all() if not alternative == sound.species]
-
-    wrong_counts = []
-    for alternative in alternatives:
-        wrong_counts.append(sum(1 for guess in Guess.query.filter_by(species=sound.species)
-                                if guess.sound.species == alternative))
-
-    weigths = [1 + count for count in wrong_counts]
-    choices = [sound.species] + random.choices(alternatives, weights=weigths, k=3)
-
-    random.shuffle(choices)
-
-    return choices
-
-
-@dataclass
-class Quiz:
-
-    sound: Sound
-    choices: List[Species]
-
-    def __post_init__(self) -> None:
-        if self.sound.species not in self.choices:
-            raise Exception('Impossible Quiz!')
-
-
-def make_quiz() -> Quiz:
-
-    sound = get_sound()
-    choices = get_choices(sound=sound)
-
-    return Quiz(sound=sound, choices=choices)
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-
-    if request.method == 'POST':
-        session['species_id_guess'] = session['species_ids_choices'][int(request.form.get('choice_index')) - 1]
-        return redirect(url_for('answer'))
-
-    quiz = make_quiz()
-
-    session['species_id_correct'] = quiz.sound.species.id
-    session['species_ids_choices'] = [species.id for species in quiz.choices]
-
-    return render_template('index.html', quiz=quiz)
-
-
-@app.route('/answer')
-def answer():
-
-    correct_species = Species.query.get(session['species_id_correct'])
-    guessed_species = Species.query.get(session['species_id_guess'])
-
-    return render_template('answer.html', correct_species=correct_species, guessed_species=guessed_species)
+    return render_template('index.html')
